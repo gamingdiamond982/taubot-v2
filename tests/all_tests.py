5a11888486ac3de2dcd73b6052be4bce22c23380
stoner <oj.nologic@gmail.com>
2024-08-08 23:32:10 +0100

    tests passing

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-
+import asyncio
 import unittest
 from backend_tests import BackendTests
 
 
 
+
+
+
+
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
